2c7097675f78c5b32b9a3cc2fb2a0411da2115e6
CarlosMoran12 <mo25003@esfe.agape.edu.sv>
2026-09-23 08:57:31 -0600

    style: forzar tema oscuro en PokedexMobile

--- PokedexMobile/src/app/_layout.tsx
+++ PokedexMobile/src/app/_layout.tsx
@@ -1,7 +1,6 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
+import { DarkTheme, ThemeProvider } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useColorScheme } from 'react-native';
-
+ 
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
 import AppTabs from '@/components/app-tabs';
 import { PokedexStoreProvider } from '@/data/pokedex-store';
@@ -9,9 +8,8 @@ import { PokedexStoreProvider } from '@/data/pokedex-store';
 SplashScreen.preventAutoHideAsync();
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={DarkTheme}>
       <PokedexStoreProvider>
         <AnimatedSplashOverlay />
         <AppTabs />
